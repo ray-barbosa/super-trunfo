342d96a647e9b3272a116fe6c0306d94ce9baace
Ray Krishna <rayanebarbosa.dev@gmail.com>
2025-10-14 23:37:59 +0000

    feature: executando comandos em GO

--- app/main.go
+++ app/main.go
@@ -2,7 +2,27 @@
 
     import "fmt"
 
+	func cadastroJogador(nome string) string {
+		return "Jogador " + nome + " cadastrado com sucesso!"
+	}
+
+	func somar(num1 int, num2 int) int {
+		return num1 + num2
+	}
+
+	func subtrair(num1 int, num2 int) int {
+		return num1 - num2
+	}
+
     func main() {
         fmt.Println(cadastroJogador("Alice"))
 		fmt.Println(cadastroJogador("Bob"))
+		fmt.Println(somar(5, 10))
+		fmt.Println(subtrair(10, 5))
 	   }
+
+
+
+
+
+
